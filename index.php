1277ee5a87e3a3f820e00a6a40120edd647f3693
pierre.K <minder309@gmail.com>
2023-03-22 13:47:21 +0800

    經歷部分完成。

--- index.php
+++ index.php
@@ -69,94 +69,148 @@
                     <div class="flex-grow-1">
                         <h3 class="mb-0">學生</h3>
                         <div class="subheading mb-3">泰山職業訓練場-PHP資料庫網頁設計</div>
-                        <pre>
-                            學習:
-                            1.php程式語言
-                            2.jq程式語言
-                            3.mysql程式語言
-                            4.bootstrap前端架構
-                        </pre>
+                        <p>
+                            學習:<br>
+                            1.php程式語言<br>
+                            2.jq程式語言<br>
+                            3.mysql程式語言<br>
+                            4.bootstrap前端架構<br>
+                        </p>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">2022/9~2023/3</span></div>
+                    <div class="flex-shrink-0"><span class="text-primary">2022/9-2023/3</span></div>
                 </div>
                 <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                     <div class="flex-grow-1">
                         <h3 class="mb-0">維修工程師</h3>
                         <div class="subheading mb-3">華茂資訊股份有限公司 </div>
-                        <pre>
-                            2.發票機調整維修。
-                            3.電腦設備問題解決。
-                            4.基礎線路建置與維修。
-                            5.costco pos 發票機 出單機，維修與調整。
-                            6.各項主管交辦事項。
-                            機調整維修。
-                        </pre>
+                        <p>
+                            1.pos機調整維修。<br>
+                            2.發票機調整維修。<br>
+                            3.電腦設備問題解決。<br>
+                            4.基礎線路建置與維修。<br>
+                            5.costco<br> pos 發票機 出單機，維修與調整。
+                            6.各項主管交辦事項。<br>
+                            機調整維修。<br>
+                        </p>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">July 2010 - December 2011</span></div>
+                    <div class="flex-shrink-0"><span class="text-primary">2021/11-2022/06</span></div>
                 </div>
                 <div class="d-flex flex-column flex-md-row justify-content-between">
                     <div class="flex-grow-1">
-                        <h3 class="mb-0">Web Design Intern</h3>
-                        <div class="subheading mb-3">Shout! Media Productions</div>
-                        <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                        <h3 class="mb-0">銷售行銷企劃</h3>
+                        <div class="subheading mb-3">惠光藥局</div>
+                        <p>1.店鋪固定清潔。<br>
+                            2.倉儲與架上管理。<br>
+                            3.銷售健康食品。<br>
+                            4.客情維護。<br>
+                            5.基本設備維護。<br>
+                            6.建置蝦皮網拍。<br>
+                            7.建置GOOGLE地圖地標。<br></p>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
+                    <div class="flex-shrink-0"><span class="text-primary">2019/12-2021/07</span></div>
                 </div>
                 <div class="d-flex flex-column flex-md-row justify-content-between">
                     <div class="flex-grow-1">
-                        <h3 class="mb-0">Web Design Intern</h3>
-                        <div class="subheading mb-3">Shout! Media Productions</div>
-                        <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                        <h3 class="mb-0">系統工程師</h3>
+                        <div class="subheading mb-3">仁通資訊股份有限公司</div>
+                        <p>
+                            1.成大醫院固定維護。<br>
+                            2.設定成大醫院電腦設定。<br>
+                            3.排除成大醫院固定線路網路問題。<br>
+                            4.參與AP專案安裝與設定。<br>
+                            5.成大AP故障排除與安裝。<br>
+                            6.基本設定SWITCH。<br>
+                            7.UTP牽線。<br>
+                            8.光纖牽線。<br>
+                            9.遠端協助排除成醫故障。<br>
+                            10.各項公司指導事項。<br>
+                        </p>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
+                    <div class="flex-shrink-0"><span class="text-primary">2018/12-2019/12</span></div>
                 </div>
                 <div class="d-flex flex-column flex-md-row justify-content-between">
                     <div class="flex-grow-1">
-                        <h3 class="mb-0">Web Design Intern</h3>
-                        <div class="subheading mb-3">Shout! Media Productions</div>
-                        <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                        <h3 class="mb-0">總務/網管</h3>
+                        <div class="subheading mb-3">林澄輝社會福利慈善事業基金會</div>
+                        <p>
+                            一.公司網路設備規劃管理。<br>
+                            &emsp; 1.雙NAS架構實現異地備份，與資料監控。<br>
+                            &emsp; 2.規劃公司防火牆系統，使用RouterOS實現三間機構網路內互通設定。節省公司內部資料互通成本。<br>
+                            &emsp; 3.用ASUS AiMesh架構內部網路達到涵蓋率90%以上，且不會斷網。<br>
+                            二.養護中心系統評估與導入。<br>
+                            &emsp;1.導入博x系統進入公司。<br>
+                            &emsp;2.建置使用說明書，教學使用。<br>
+                            &emsp;3.協助博x公司排除BUG提升使用體驗。<br>
+                            &emsp;4.導入無線生命徵象系統監控系統，減低人力成本。<br>
+                            三.導入JANDI<br>
+                            &emsp;1.不再有離職員工留在群組內部問題。<br>
+                            &emsp;2.公私群組正式分開，減少員工抱怨。<br>
+                            &emsp;3.內部資料互傳安全度上升。<br>
+                            四.使用GOOGLE進行低成本廣告播送。<br>
+                            &emsp;1.使用Google<br> AdWords進行廣告。
+                            &emsp;2.建置公司GOOGLE地圖地標。<br>
+                            五.日照中心與長照A據點整體設備與網路架構。<br>
+                            &emsp;1.與室內設計師討論日照中心設計。<br>
+                            &emsp;2.評估水電使用量與預計使用設備擺放位置。<br>
+                            &emsp;3.設備採購，與使用人數粗估。<br>
+                            &emsp;4.消防設備討論與預防失火。<br>
+                        </p>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
+                    <div class="flex-shrink-0"><span class="text-primary">2016/07-2018/07</span></div>
                 </div>
                 <div class="d-flex flex-column flex-md-row justify-content-between">
                     <div class="flex-grow-1">
-                        <h3 class="mb-0">Web Design Intern</h3>
-                        <div class="subheading mb-3">Shout! Media Productions</div>
-                        <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                        <h3 class="mb-0">總務</h3>
+                        <div class="subheading mb-3">臨安養護中心</div>
+                        <p>
+                            一.重要設備採購評估與實施。<br>
+                            &emsp; 1.50kw熱泵評估採購。<br>
+                            &emsp;2.特殊輔具採購。<br>
+                            &emsp;&emsp;1.沐浴設施全躺式洗頭椅採購。<br>
+                            &emsp;&emsp;2.日本水壓式無電洗澡椅系統設備採購。<br>
+                            &emsp;&emsp;3.日本製特殊可調式桌椅系統採購。<br>
+                            &emsp;&emsp;4.日本製帕金森氏症專用碗盤與餐具評估採購。<br>
+                            &emsp;&emsp;5.醫療用呼叫系統評估採購。<br>
+                            &emsp;&emsp;6.特殊用醫療坐墊評估採購。<br>
+                            &emsp;&emsp;7.醫療用氣墊床採購。<br>
+                            二.導入google雲端物品申請與採購申請<br>
+                            &emsp;1.減少紙本與資訊遺失機率。<br>
+                            &emsp;2.倉庫系統重整，物品分類明確，與入出庫數量核對。<br>
+                            三.公文線上搜尋與行事曆整合系。<br>
+                            &emsp;1.利用google雲端硬碟建置線上公文查詢系統，減少外點機構查詢公文成本<br>
+                            &emsp;2.建置google協作平台，進行資訊整合可迅速知到誰請假與今日各項進行事件<br>
+                            三.日照中心防水工程與屋頂陽台工程評估與施做。<br>
+                            &emsp;1.日照中心陽台屋頂進行花園改造，架空花圃處裡。<br>
+                            &emsp;2.排水設施設備檢修。<br>
+                            &emsp;3.室內漏水維修評估處理。<br>
+                            三.2016美濃大地震後緊急應變與處理。<br>
+                            &emsp;1.2016地震供水系統受損水塔破裂緊急應變處理。<br>
+                            &emsp;2.機構因天災供水系統停水,緊急規劃實施長達21天應變處理得宜。<br>
+                        </p>
                     </div>
                     <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
                 </div>
-                <div class="d-flex flex-column flex-md-row justify-content-between">
-                    <div class="flex-grow-1">
-                        <h3 class="mb-0">Web Design Intern</h3>
-                        <div class="subheading mb-3">Shout! Media Productions</div>
-                        <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
-                    </div>
-                    <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
-                </div>
+                
             </div>
         </section>
         <hr class="m-0" />
         <!-- 教育-->
         <section class="resume-section" id="education">
             <div class="resume-section-content">
-                <h2 class="mb-5">Education</h2>
+                <h2 class="mb-5">教育</h2>
                 <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                     <div class="flex-grow-1">
-                        <h3 class="mb-0">University of Colorado Boulder</h3>
-                        <div class="subheading mb-3">Bachelor of Science</div>
-                        <div>Computer Science - Web Development Track</div>
-                        <p>GPA: 3.23</p>
+                        <h3 class="mb-0">南台科技大學</h3>
+                        <div class="subheading mb-3">資訊工程學系 大學畢業</div>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">August 2006 - May 2010</span></div>
+                    <div class="flex-shrink-0"><span class="text-primary">2017/07-2021/07</span></div>
                 </div>
                 <div class="d-flex flex-column flex-md-row justify-content-between">
                     <div class="flex-grow-1">
-                        <h3 class="mb-0">James Buchanan High School</h3>
-                        <div class="subheading mb-3">Technology Magnet Program</div>
-                        <p>GPA: 3.56</p>
+                        <h3 class="mb-0">私立嘉南藥理科技大學</h3>
+                        <div class="subheading mb-3">社會工作系 大學畢業</div>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">August 2002 - May 2006</span></div>
+                    <div class="flex-shrink-0"><span class="text-primary">2009/07-2013/07</span></div>
                 </div>
             </div>
         </section>
@@ -164,21 +218,13 @@
         <!-- 技能-->
         <section class="resume-section" id="skills">
             <div class="resume-section-content">
-                <h2 class="mb-5">Skills</h2>
+                <h2 class="mb-5">技能</h2>
                 <div class="subheading mb-3">Programming Languages & Tools</div>
                 <ul class="list-inline dev-icons">
                     <li class="list-inline-item"><i class="fab fa-html5"></i></li>
                     <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
                     <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-angular"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-react"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-sass"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-less"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-gulp"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-grunt"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                    
                 </ul>
                 <div class="subheading mb-3">Workflow</div>
                 <ul class="fa-ul mb-0">
