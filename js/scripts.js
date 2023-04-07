352119701e01e519ba9c95841e2904f46a00bdd9
Pierre.K <minder309@gmail.com>
2023-04-07 13:13:17 +0800

    滑入顯示設定成功。

--- js/scripts.js
+++ js/scripts.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
             // 視窗高度+滑動頂端高度，就是現在滑多少高度。
             let scrollSumWindow_height=window_height+document_hight;
             console.log("目前視窗觀看高度",scrollSumWindow_height)
-            if($(this).offset().top<scrollSumWindow_height){
+            if($(this).offset().top<scrollSumWindow_height-200){
                 console.log("顯示")
                 $(this).addClass("show");
             }else{
